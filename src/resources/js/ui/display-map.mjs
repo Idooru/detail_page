b30552fbbed7c5dcdc31827fdcc4b8011639fd24
idooru <shere1765@gmail.com>
2023-10-25 21:59:54 +0900

    지도를 표시하는 함수

---
+++ src/resources/js/ui/display-map.mjs
@@ -0,0 +1,33 @@
+export const displayMap = (param) => {
+  const { kakao } = window;
+
+  const container = document.querySelector("#map .item_info_body");
+  const options = {
+    center: new kakao.maps.LatLng(param.y, param.x),
+    level: 1,
+  };
+
+  const map = new kakao.maps.Map(container, options);
+
+  const imageSrc =
+    "https://cdn.icon-icons.com/icons2/2104/PNG/512/map_location_icon_129048.png"; // 마커이미지의 주소입니다
+  const imageSize = new kakao.maps.Size(50, 52); // 마커이미지의 크기입니다
+  const imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+  const markerImage = new kakao.maps.MarkerImage(
+    imageSrc,
+    imageSize,
+    imageOption,
+  );
+
+  const markerPosition = new kakao.maps.LatLng(param.y, param.x); // 마커가 표시될 위치입니다
+
+  // 마커를 생성합니다
+  const marker = new kakao.maps.Marker({
+    position: markerPosition,
+    image: markerImage, // 마커이미지 설정
+  });
+
+  // 마커가 지도 위에 표시되도록 설정합니다
+  marker.setMap(map);
+};
