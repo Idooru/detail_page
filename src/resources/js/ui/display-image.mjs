e52f191573447effdd28819f58c1b54dae76810e
idooru <shere1765@gmail.com>
2023-10-25 21:59:26 +0900

    이미지를 표시하는 함수

---
+++ src/resources/js/ui/display-image.mjs
@@ -0,0 +1,17 @@
+export const displayImage = (param) => {
+  const pictureBodyEle = document.querySelector("#picture .item_info_body");
+
+  if (!param.place_image) {
+    const h3Ele = document.createElement("h3");
+    h3Ele.style.color = "#99958b";
+    h3Ele.innerText = "아직 사진이 없습니다.";
+
+    pictureBodyEle.style.background = "#cfcbc2";
+    pictureBodyEle.style.display = "flex";
+    pictureBodyEle.style.justifyContent = "center";
+    pictureBodyEle.style.alignItems = "center";
+    pictureBodyEle.appendChild(h3Ele);
+  } else {
+    alert("dsd");
+  }
+};
