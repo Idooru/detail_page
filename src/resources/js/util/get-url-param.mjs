f425b91cce270ba2239736917c58cff204195d13
Idooru <shere1765@gmail.com>
2023-10-26 11:21:37 +0900

    식당 위치와 나의 위치를 구분하여 가져오기

--- src/resources/js/util/get-url-param.mjs
+++ src/resources/js/util/get-url-param.mjs
@@ -2,14 +2,16 @@ export const getUrlParam = () => {
   const urlParams = new URLSearchParams(window.location.search);
 
   return {
-    place_image: urlParams.get("place_image"),
-    place_name: urlParams.get("place_name"),
-    address_name: urlParams.get("address_name"),
+    placeImage: urlParams.get("place_image"),
+    placeName: urlParams.get("place_name"),
+    addressName: urlParams.get("address_name"),
     phone: urlParams.get("phone"),
-    category_name: urlParams.get("category_name"),
-    place_url: urlParams.get("place_url"),
+    categoryName: urlParams.get("category_name"),
+    placeUrl: urlParams.get("place_url"),
     review: urlParams.get("review"),
-    x: urlParams.get("x"),
-    y: urlParams.get("y"),
+    shopX: urlParams.get("shop_x"),
+    shopY: urlParams.get("shop_y"),
+    myX: urlParams.get("my_x"),
+    myY: urlParams.get("my_y"),
   };
 };
